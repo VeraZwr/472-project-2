36bcfc74f28b2e0e612cb4e7a069ee9ac24c8237
VeraZwr <vera.zwr@hotmail.com>
2020-03-22 16:42:37 -0400

    first commit

---
+++ project2.1.py
@@ -0,0 +1,31 @@
+import re
+v0 = {"eu": [], "ca": [], "gl": [], "es": [], "en": [], "pt": []}
+unigram = dict()
+
+
+def parseFile(file):
+    f = open(file, "r", encoding="iso8859-2")
+    text = f.read()
+    f.close()
+    return text
+
+def generate_v(text, v_type):
+    regex = re.compile('[^a-zA-Z]')
+    text_list = text.splitlines()
+    for text_line in text_list:
+        content = text_line.split("\t")
+        if len(content) == 4:
+            if v_type == 0:
+                v0[content[2]].append((regex.sub('*', content[3])).lower())
+            elif v_type == 1:
+                v0[content[2]].append((regex.sub('*', content[3])))
+            elif v_type == 2:
+                content[3] = [char for char in content[3]]
+                for i in range(len(content[3])):
+                    if not content[3][i].isalpha():
+                        content[3][i] = '*'
+                v0[content[2]].append(''.join([str(elem) for elem in content[3]]))
+    return print(v0["eu"])
+
+
+generate_v(parseFile("./training-tweets.txt"),2)
