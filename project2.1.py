677b01ffed307b16710072de2e994a185d6ad044
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-04-04 16:35:09 -0400

    clean text

--- project2.1.py
+++ project2.1.py
@@ -34,4 +34,31 @@ def generate_v(text, v_type):
 
 generate_v(parseFile("./training-tweets.txt"), 0)
 # unigram.generate_unigram(v0, 0, 0.1)
-bigram.generate_bigram(v0, 0, 0.1)
+bigram_model = bigram.generate_bigram(v0, 0, 0.1)
+
+
+def clean(v):
+    clean_text = []
+    regex = re.compile('[^a-zA-Z]')
+    test_file = open("test-tweets-given.txt", "r", encoding="utf-8")
+    text = test_file.read()
+    text_lines = text.splitlines()
+    for line in text_lines:
+        split_content = line.split("\t")
+        content = ""
+        if len(split_content) == 4:
+            if v == 0:
+                content = (regex.sub('*', split_content[3])).lower()
+            elif v == 1:
+                content = regex.sub('*', split_content[3])
+            elif v == 2:
+                chars = [char for char in split_content[3]]
+                for i in range(len(chars)):
+                    if not chars[i].isalpha():
+                        chars[i] = '*'
+                content = ''.join([str(elem) for elem in chars])
+        clean_text.append(content)
+    return clean_text
+
+
+# trace_file = open("trace_" + str(v) + "_" + str(n) + "_" + str(d) + ".txt", "w")
